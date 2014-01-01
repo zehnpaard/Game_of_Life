5a8f39ebd080402456592785e98b976854f0fcbb
zehnpaard <zehnpaard@googlemail.com>
2014-01-01 00:00:52 +0000

    board.py updated so next_board passes tests
    Implemented next_board so that it passes tests

--- board.py
+++ board.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import itertools
+
 """ Implements functions related to the creation, loading and updating
 of the game board.
 """
@@ -43,7 +45,41 @@ def next_board(board):
     and diagonally adjacent.
     Edges of the board are treated as "dead".
     """
-    pass
+    max_row, max_col = len(board), len(board[0])
+    new_board = create_board(max_row, max_col)
+
+    coords = itertools.product(xrange(max_row), xrange(max_col))
+    for cell_row, cell_col in coords:
+        surrounding_alive = sum_neighbors(cell_row, cell_col, board)
+        if board[cell_row][cell_col]:
+            if surrounding_alive in (2, 3):
+                new_board[cell_row][cell_col] = 1
+        else:
+            if surrounding_alive == 3:
+                new_board[cell_row][cell_col] = 1
+
+    return new_board
+
+def sum_neighbors(cell_row, cell_col, board):
+    """ Return sum of values of the 8 surrounding neighbors of a cell.
+    Same as the number of alive neighbors of the cell.
+    """
+    max_row, max_col = len(board), len(board[0])
+
+    valid_coords = ((i, j) for i in valid_range(cell_row, max_row)
+                           for j in valid_range(cell_col, max_col))
+
+    neighbor_vals = (board[row][col] for row, col in valid_coords)
+    return sum(neighbor_vals) - board[cell_row][cell_col]
+
+
+def valid_range(cell_index, side_count):
+    """ Returns an xrange object that yields upto 9 cells, offset by
+    -1 and 1 for each dimension, except where that hits against the
+    borders of the board, in which case the valid neighbors is truncated.
+    """
+    return xrange(max(0, cell_index-1), min(cell_index+2, side_count)) 
+
 
 def main():
     pass
