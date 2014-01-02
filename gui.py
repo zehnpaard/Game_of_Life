221f75c38345d4523778c12f06dc27bae6b5e2eb
zehnpaard <zehnpaard@googlemail.com>
2014-01-02 12:34:05 +0000

    Implementation ver. 2
    Implemented App object in the main module
    - mainloop and tick scheduling handled at the top level, rather than by
    the GUI code
    - App object manages interaction between the board logic and GUI display
    - Eliminated global variable for board data, and the dynamic injection
    of functions into the gui App object
    - App and GuiApp strongly coupled, preferably ought to be modularized
    better
    
    Separately, a bug in the GUI code allowing the 'flipping' of an area
    outside of the grid by clicking on a region 1-pixel wide was fixed.

--- gui.py
+++ gui.py
@@ -8,92 +8,47 @@ from Tkinter import *
 import ttk
 
 
-class App():
-    """ Main GUI class
+class GuiApp(ttk.Frame):
+    """ Main GUI class that inherits from ttk.Frame
     """
-    def __init__(self, rows=70, cols=70, speed=100):
-        self.root = Tk()
-        mainframe = ttk.Frame(self.root)
-        mainframe.grid(row=0, column=0, sticky=(N, W, S, E))
+    def __init__(self, app):
+        ttk.Frame.__init__(self, app.root)
 
-        self.active = False
-
-        self.rows = rows
-        self.cols = cols
-
-        self.speed = speed
+        self.app = app
 
         # Create canvas to display game state and allow interactive edits
-        self.canvas = BoardCanvas(mainframe, self)
+        self.canvas = BoardCanvas(self, self.app)
         self.canvas.grid(row=0, column=0, rowspan=20, 
                 sticky=(N, W, S, E))
 
         # Create button to activate/deactivate game
-        self.activate_button = ActivateButton(mainframe, self)
+        self.activate_button = ActivateButton(self, self.app)
         self.activate_button.grid(row=0, column=1, columnspan=2,
                 sticky=(W, E))
 
         # Create entry box to set game speed
-        self.speed_label = ttk.Label(mainframe)
+        self.speed_label = ttk.Label(self)
         self.speed_label.configure(text="Tick Length (ms):")
         self.speed_label.grid(row=1, column=1, sticky=(N, W))
         self.speed_text = StringVar()
-        self.speed_text.set(self.speed)
+        self.speed_text.set(self.app.speed)
 
-        self.speed_entry = ttk.Entry(mainframe)
+        self.speed_entry = ttk.Entry(self)
         self.speed_entry.configure(textvariable=self.speed_text)
         self.speed_entry.grid(row=1, column=2, sticky=(N, W))
 
         # Display current generation count
-        self.generation = 0
         self.gen_str = 'Generation #{0:6d}'
         self.gen_text = StringVar()
-        self.gen_text.set(self.gen_str.format(self.generation))
+        self.update_generation_display(self.app.generation)
 
-        self.gen_label = ttk.Label(mainframe)
+        self.gen_label = ttk.Label(self)
         self.gen_label.configure(textvariable=self.gen_text)
         self.gen_label.grid(row=2, column=1, columnspan=2,
                 sticky=(N, W))
 
-
-    def update_board_data(self):
-        """ Callback to be used to update board data by one generation
-        """
-        pass
-
-    def get_live_cell_data(self):
-        """ Request to get a generator that yields all the coords of
-        cells that are currently alive in the board data
-        """
-        pass
-
-    def update_cell_in_board_data(self, row, col):
-        """ 'Flip' the value of one cell in the board data representation
-        such that a dead cell becomes alive, and an alive cell becomes
-        dead.
-        """
-        pass
-
-    def tick(self):
-        """ If game state is active, update data by one generation,
-        represent that on canvas, update generation count and show,
-        and schedule another run of this function in the future
-        """
-        if self.active:
-            self.update_board_data()
-            self.canvas.clear_live()
-            for row, col in self.get_live_cell_data():
-                self.canvas.turn_cell_active(row, col)
-            self.generation += 1
-            self.gen_text.set(self.gen_str.format(self.generation))
-            self.root.after(self.speed, self.tick)
-
-    def run(self):
-        """ Run the first tick (which will schedule further ticks if game
-        state is active), and then run mainloop.
-        """
-        self.tick()
-        self.root.mainloop()
+    def update_generation_display(self, generation):
+        self.gen_text.set(self.gen_str.format(generation))
 
 
 
@@ -128,8 +83,8 @@ class BoardCanvas(Canvas):
                                fill="grey")
 
 
-
         self.bind("<Button-1>", self.update_clicked_cell)
+
 
     def update_clicked_cell(self, event):
         """If the user clicked within the grid when the game state is
@@ -139,22 +94,24 @@ class BoardCanvas(Canvas):
         If the user clicked outside the grid or the game state is active,
         do nothing.
         """
-        if self.app.active or self.outside_range(event):
+        if self.app.is_active() or self.outside_range(event):
             return
 
         row, col = self.get_coord(event)
-        if self.coords((row, col)):
-            self.turn_cell_inactive(row, col)
+        if self.coords((row, col)):     # coords method inherited from Canvas
+            self.display_cell_as_dead(row, col)
         else:
-            self.turn_cell_active(row, col)
+            self.display_cell_as_alive(row, col)
         self.app.update_cell_in_board_data(row, col)
+
 
     def outside_range(self, event):
         """ Determine if the event occurred outside of the
         game board grid.
         """
-        return event.x < 10 or event.x > self.width-10 or \
-                event.y < 10 or event.y > self.height-10
+        return event.x < 10 or event.x > self.width-11 or \
+                event.y < 10 or event.y > self.height-11
+
 
     def get_coord(self, event):
         """ Convert the event's x,y to the row/col of the board
@@ -162,7 +119,8 @@ class BoardCanvas(Canvas):
         col, row = (event.x - 10) / self.block, (event.y - 10) / self.block
         return row, col
 
-    def turn_cell_active(self, row, col):
+
+    def display_cell_as_alive(self, row, col):
         """ Color the specified square in the grid to red
         """
         self.create_rectangle(11 + self.block*col,
@@ -172,10 +130,12 @@ class BoardCanvas(Canvas):
                               fill='red',
                               tag=((row, col),'live'))
 
-    def turn_cell_inactive(self, row, col):
+
+    def display_cell_as_dead(self, row, col):
         """ Uncolor the specified square in the grid
         """
         self.delete((row, col))
+
 
     def clear_live(self):
         """ Clear all live cells on grid
@@ -202,16 +162,14 @@ class ActivateButton(ttk.Button):
         """ If current game state is not active, then activate.
         Otherwise deactivate.
         """
-        if self.app.active:
-            self.app.active = False
+        if self.app.is_active():
+            self.app.deactivate_game()
             self.text.set('Start')
         else:
             try:
-                self.app.speed = int(self.app.speed_text.get())
+                self.app.activate_game()
             except ValueError:
                 return
-            self.app.active = True
-            self.app.tick()
             self.text.set('Stop')
 
 
--- main.py
+++ main.py
@@ -6,50 +6,96 @@ logic held under the board.py module and the display
 and loop logic held under gui.py module
 """
 
+import Tkinter
 import board
 import gui
 
-SIDE = 80
 
-# global variable
-game_board = board.create_board(SIDE, SIDE)
 
-def main():
-    app = gui.App(SIDE, SIDE)
+class App(object):
+    """ Main application handling event loop and
+    gluing together the board logic and GUI display.
+    """
+    def __init__(self, rows, cols, speed):
+        self.root = Tkinter.Tk()
 
-    # Implement the "update_cell_in_board_data",
-    # "update_board_data" and "get_live_cell_data" functions
-    # required by the gui, using the global game_board and
-    # functions contained in board.py.
-    # Once created, insert into the App object created.
-    def update_cell(row, col):
+        self.rows = rows
+        self.cols = cols
+
+        self.speed = speed 
+
+        self.generation = 0
+        self._active = False
+
+
+        self.game_board = board.create_board(self.rows, self.cols)
+        self.gui = gui.GuiApp(self)
+        self.gui.pack()
+
+
+    def _tick(self):
+        """ If game state is active:
+        1) update data by one generation,
+        2) represent that on gui canvas,
+        3) update generation count and display, and
+        4) schedule another run of this function in the future
+        If game state is not active, do nothing
+        """
+        if self._active:
+            self.game_board = board.next_board(self.game_board)
+
+
+            self.gui.canvas.clear_live()
+            for row, col in board.get_coords(self.game_board):
+                self.gui.canvas.display_cell_as_alive(row, col)
+
+
+            self.generation += 1
+            self.gui.update_generation_display(self.generation)
+
+            self.root.after(self.speed, self._tick)
+
+
+    def update_cell_in_board_data(self, row, col):
         """ 'Flip' the value in cell on the board data
         """
-        global game_board
-        if board.get_value(game_board, row, col):
-            board.set_dead(game_board, row, col)
+        if board.get_value(self.game_board, row, col):
+            board.set_dead(self.game_board, row, col)
         else:
-            board.set_alive(game_board, row, col)
-    app.update_cell_in_board_data = update_cell
+            board.set_alive(self.game_board, row, col)
 
-    def update_board_data():
-        """ Update the game board data by one generation
+    def activate_game(self):
+        """ 1) Set the game speed to match the entry,
+        2) make the game state active, and
+        3) run the next tick (which will then schedule the 
+        next tick by itself.
+        If inputted game speed is not valid, raise an Error
         """
-        global game_board
-        game_board = board.next_board(game_board)
-    app.update_board_data = update_board_data
+        try:
+            self.speed = int(self.gui.speed_text.get())
+        except ValueError:
+            raise ValueError
+        self._active = True
+        self._tick()
 
-    def get_live_cell_data():
-        """ Return a generator with the coordinates of all
-        live cells in the game_board.
+    def deactivate_game(self):
+        self._active = False
+
+    def is_active(self):
+        return self._active
+
+
+    def run(self):
+        """ Run the first tick (which will schedule further ticks if game
+        state is active), and then run mainloop.
         """
-        global game_board
-        return board.get_coords(game_board)
-    app.get_live_cell_data = get_live_cell_data
+        self._tick()
+        self.root.mainloop()
 
-    # Run mainloop
+
+def main():
+    app = App(rows=80, cols=80, speed=100)
     app.run()
-
 
 
 
